e62165630ff61cf2664d7a07f38c94c1e3736259
EbinReji18 <ebinofficial18@gmail.com>
2024-11-13 20:17:23 +0530

    third

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-// require('dotenv').config()
+require('dotenv').config()
 
-// const express = require('express')
-// const cors = require('cors')
-// const router=require('./Routes/route')
-// require('./Connection/db')
+const express = require('express')
+const cors = require('cors')
+const router=require('./Routes/route')
+require('./Connection/db')
 
-
-// const empServer = express();
-
-
-// empServer.use(cors())
-// empServer.use(express.json())
-// empServer.use(router)
-
-// const PORT=3000 || process.env.PORT
-
-// empServer.listen(PORT,()=>{
-//     console.log(`Server is running at :  ${PORT}`);
-// });
-
-require('dotenv').config();
-
-const express = require('express');
-const cors = require('cors');
-const router = require('./Routes/route');
-require('./Connection/db');
 
 const empServer = express();
 
-empServer.use(cors({
-  origin: 'https://employee-front-henna.vercel.app', // Only allows requests from this origin
-}));
 
-empServer.use(express.json());
-empServer.use(router);
+empServer.use(cors())
+empServer.use(express.json())
+empServer.use(router)
 
-const PORT = process.env.PORT || 3000;
+const PORT=3000 || process.env.PORT
 
-empServer.listen(PORT, () => {
-  console.log(`Server is running at: ${PORT}`);
+empServer.listen(PORT,()=>{
+    console.log(`Server is running at :  ${PORT}`);
 });
+
+// require('dotenv').config();
+
+// const express = require('express');
+// const cors = require('cors');
+// const router = require('./Routes/route');
+// require('./Connection/db');
+
+// const empServer = express();
+
+// empServer.use(cors({
+//   origin: 'https://employee-front-henna.vercel.app', // Only allows requests from this origin
+// }));
+
+// empServer.use(express.json());
+// empServer.use(router);
+
+// const PORT = process.env.PORT || 3000;
+
+// empServer.listen(PORT, () => {
+//   console.log(`Server is running at: ${PORT}`);
+// });
